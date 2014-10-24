171bd00121bf30e07235617af9846172ad438980
leewe626 <leewe@Remus.local>
2014-10-24 16:42:37 -0700

    Exercise 6 Completed

---
+++ sorted_data.py
@@ -0,0 +1,21 @@
+# Opening the file and assigning the restaurant name and rating to lists.    
+
+name = []
+score = []
+
+f = open("scores.txt")
+
+for line in f:
+    entries = line.split(':')
+    line2 = line.replace('\n', '') 
+    name.append(entries[0])
+    score.append(entries[1])
+
+ratings_dict = dict(zip(name,score))
+
+keys_list = ratings_dict.keys()
+
+keys_list.sort()
+
+for key in keys_list:
+    print "Restaurant %s received a rating of %s" % (key, ratings_dict[key])
